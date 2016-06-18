a211395025c58346431ef644deda731e93dfe601
Rodnei3009 <rbrassoroto@yahoo.com.br>
2016-06-18 17:29:17 -0300

    45
    45

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('dateController', function($scope) {
-    $scope.dateqq = '18-jun-2016';
+    $scope.dateAtual = moment(new Date()).format('DD-MMM-YYYY');
+
+    $scope.before = function() {
+        $scope.dateAtual = moment($scope.dateAtual, "DD-MMM-YYYY").subtract(1, 'day').format('DD-MMM-YYYY');
+    };
+
+    $scope.after = function() {
+        $scope.dateAtual = moment($scope.dateAtual, "DD-MMM-YYYY").add(1, 'day').format('DD-MMM-YYYY');
+    };
+
 });
+
+
