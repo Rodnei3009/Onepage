d1c40cc2d9b252a84d597629c331875f54d05fe2
Rodnei3009 <rbrassoroto@yahoo.com.br>
2016-06-19 13:16:28 -0300

    we
    we

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.controller('dateController', function($scope) {
     $scope.dateAtual = moment(new Date()).format('DD-MMM-YYYY');
+    $scope.monthAtual = moment(new Date()).format('MMM-YYYY');
 
     $scope.before = function() {
         $scope.dateAtual = moment($scope.dateAtual, "DD-MMM-YYYY").subtract(1, 'day').format('DD-MMM-YYYY');
@@ -13,4 +14,23 @@ myApp.controller('dateController', function($scope) {
 
 });
 
+myApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/monthly.html',
+            controller: 'monthlyController'
+        })
+        .when('/daily', {
+            templateUrl: 'pages/daily.html',
+            controller: 'dailyController'
+        });
+ 
+});
 
+myApp.controller('dailyController', function($scope) {
+    $scope.pageClass = 'page-daily';
+});
+
+myApp.controller('monthlyController', function($scope) {
+    $scope.pageClass = 'page-monthly';
+});
