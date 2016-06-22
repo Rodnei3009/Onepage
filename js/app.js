268b9fd891220c6684d86f098e43b72529d48676
Rodnei3009 <rbrassoroto@yahoo.com.br>
2016-06-22 17:34:12 -0300

    vfunc
    vfunc

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate']);
+
+myApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/daily.html',
+            controller: 'dailyController'
+        })
+        .when('/monthly', {
+            templateUrl: 'pages/monthly.html',
+            controller: 'monthlyController'
+        });
+ 
+});
+
+myApp.controller('dailyController', function($scope) {
+    $scope.pageClass = 'page-daily';
+    //alert('buceta gorda');
+});
+
+myApp.controller('monthlyController', function($scope) {
+    $scope.pageClass = 'page-monthly';
+    //alert('buceta gorda2');
+});
 
 myApp.controller('dateController', function($scope) {
     $scope.dateAtual = moment(new Date()).format('DD-MMM-YYYY');
@@ -6,31 +29,12 @@ myApp.controller('dateController', function($scope) {
 
     $scope.before = function() {
         $scope.dateAtual = moment($scope.dateAtual, "DD-MMM-YYYY").subtract(1, 'day').format('DD-MMM-YYYY');
+        $scope.monthAtual = moment($scope.monthAtual, "MMM-YYYY").subtract(1, 'month').format('MMM-YYYY');
     };
 
     $scope.after = function() {
         $scope.dateAtual = moment($scope.dateAtual, "DD-MMM-YYYY").add(1, 'day').format('DD-MMM-YYYY');
+        $scope.monthAtual = moment($scope.monthAtual, "MMM-YYYY").add(1, 'month').format('MMM-YYYY');
     };
 
-});
-
-myApp.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/monthly.html',
-            controller: 'monthlyController'
-        })
-        .when('/daily', {
-            templateUrl: 'pages/daily.html',
-            controller: 'dailyController'
-        });
- 
-});
-
-myApp.controller('dailyController', function($scope) {
-    $scope.pageClass = 'page-daily';
-});
-
-myApp.controller('monthlyController', function($scope) {
-    $scope.pageClass = 'page-monthly';
 });
